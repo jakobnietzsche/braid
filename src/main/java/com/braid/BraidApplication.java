2f67147f664567aa2ab3705f79d7bf169f0bbf1b
Lockmyre <51970698+jakobnietzsche@users.noreply.github.com>
2023-07-18 13:45:37 -0500

    initial commit

---
+++ src/main/java/com/braid/BraidApplication.java
@@ -0,0 +1,13 @@
+package com.braid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/braid/BraidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
